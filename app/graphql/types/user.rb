42d3ce0b86842f2d4126f1a02327a0e720f3ed76
Eric Kenney <e.kenney@contactelevation.com>
2020-08-22 21:55:03 -0600

    Add return url and refresh

--- app/graphql/types/user.rb
+++ app/graphql/types/user.rb
@@ -1,4 +1,13 @@
 class Types::User < Types::ActiveRecordObject
   field :listings, [Types::Listing], null: false
-  field :stripe_account, Types::StripeAccount, null: false
+  field :stripe_account, Types::StripeAccount, null: false do
+    argument :refresh, Boolean, required: false
+  end
+  def stripe_account(refresh: false)
+    if refresh
+      object.stripe_account.refresh
+    end
+
+    object.stripe_account
+  end
 end
--- app/models/stripe_account.rb
+++ app/models/stripe_account.rb
@@ -5,10 +5,30 @@ class StripeAccount < ApplicationRecord
     Stripe::AccountLink.create(
       {
         account: token,
-        refresh_url: 'https://example.com/reauth',
-        return_url: 'https://example.com/return',
+        refresh_url: refresh_url,
+        return_url: return_url,
         type: 'account_onboarding'
       }
     ).url
+  end
+
+  def refresh
+    acct = Stripe::Account.retrieve(token)
+
+    update(charges_enabled: acct.charges_enabled)
+  end
+
+  private
+
+  def return_url
+    "#{
+      Rails.application.credentials.dig(:app, :client_url)
+    }/account/verification"
+  end
+
+  def refresh_url
+    "#{
+    Rails.application.credentials.dig(:app, :client_url)
+    }/account/refresh"
   end
 end
