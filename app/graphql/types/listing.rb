08b1bb9a97accdb7ce88863482f2c061142afbca
Eric Kenney <e.kenney@contactelevation.com>
2020-09-28 22:53:20 -0600

    Show my listings

--- app/graphql/types/listing.rb
+++ app/graphql/types/listing.rb
@@ -2,4 +2,6 @@ class Types::Listing < Types::ActiveRecordObject
   field :title, String, null: false
   field :description, String, null: false
   field :image_urls, [String], null: false
+
+  field :player, Types::Player, null: false
 end
