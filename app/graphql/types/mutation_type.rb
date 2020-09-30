a61744ab08fa4fa4bbaca267fd25e8bf44b48cd2
Eric Kenney <e.kenney@contactelevation.com>
2020-09-29 22:34:53 -0600

    Add acceptOffer mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,6 +1,9 @@
 class Types::MutationType < Types::BaseObject
   field :save_listing, mutation: Mutations::SaveListing
+
   field :save_offer, mutation: Mutations::SaveOffer
+  field :accept_offer, mutation: Mutations::AcceptOffer
+
   field :track_interest, mutation: Mutations::TrackInterest
   field :save_profile_picture, mutation: Mutations::SaveProfilePicture
 end
