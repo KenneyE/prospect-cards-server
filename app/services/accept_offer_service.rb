fd17ab15e0612cb2ff84ffad95482cc69dc5fd5e
Eric Kenney <e.kenney@contactelevation.com>
2020-10-18 22:00:50 -0600

    Refactor accept in AcceptOfferService

--- app/services/accept_offer_service.rb
+++ app/services/accept_offer_service.rb
@@ -6,9 +6,7 @@ class AcceptOfferService
   end
 
   def accept
-    unless offer.listing.available?
-      raise(Errors::UserInputError, 'Cannot accept already pending offer')
-    end
+    _verify_availability
 
     # Prevent accepting another offer while we process this one
     offer.listing.update(status: :pending_sale)
@@ -16,12 +14,22 @@ class AcceptOfferService
     intent = Stripe::PaymentIntent.capture(offer.payment_intent_id)
 
     offer.listing.update(status: :sold) if intent.status == 'succeeded'
-
     offer
-
   rescue ::Stripe::StripeError => e
+    _handle_stripe_error(e)
+  end
+
+  private
+
+  def _handle_stripe_error(error)
     offer.listing.update(status: :available)
-    offer.errors.add(:base, e.message)
+    offer.errors.add(:base, error.message)
     offer
+  end
+
+  def _verify_availability
+    return if  offer.listing.available?
+
+    raise(Errors::UserInputError, 'Cannot accept already pending offer')
   end
 end
