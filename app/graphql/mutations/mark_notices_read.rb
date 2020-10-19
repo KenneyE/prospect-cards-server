ca9c84095f8175acd4cc53b0e48bbd6a6700dbaf
Eric Kenney <e.kenney@contactelevation.com>
2020-10-19 14:51:12 -0600

    Don't require confirmation

--- app/graphql/mutations/mark_notices_read.rb
+++ app/graphql/mutations/mark_notices_read.rb
@@ -1,8 +1,6 @@
 class Mutations::MarkNoticesRead < Mutations::BaseMutation
   field :success, Boolean, null: false
   def resolve
-    require_confirmation!
-
     current_user.unread_notices.update_all(read_at: Time.current)
 
     { success: true }
