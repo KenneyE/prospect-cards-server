c66b3da2a6782cb544c34184b4ecb21a46282141
Eric Kenney <e.kenney@contactelevation.com>
2020-11-05 21:46:37 -0700

    Address fields on user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_20_035707) do
+ActiveRecord::Schema.define(version: 2020_11_06_044540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -241,6 +241,12 @@ ActiveRecord::Schema.define(version: 2020_10_20_035707) do
     t.text "stripe_account_id"
     t.text "stripe_customer_id"
     t.boolean "admin", default: false, null: false
+    t.text "name"
+    t.text "street1"
+    t.text "street2"
+    t.text "city"
+    t.text "state"
+    t.text "zip"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
