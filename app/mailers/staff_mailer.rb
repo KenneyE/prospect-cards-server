f9c11c56ef3ad6adcba29917e5682874d98c5c58
Eric Kenney <e.kenney@contactelevation.com>
2020-11-13 22:18:57 -0700

    Send reported emails to me too

--- app/mailers/staff_mailer.rb
+++ app/mailers/staff_mailer.rb
@@ -2,7 +2,7 @@ class StaffMailer < ApplicationMailer
   def reported_listing(report_id)
     @report = ListingReport.find(report_id)
     mail(
-      to: 'e.kenney@prospect.cards',
+      to: 'e.kenney@prospect.cards,ekenney5@gmail.com',
       subject: '[ACTION REQUIRED] Listing Reported',
     )
   end
